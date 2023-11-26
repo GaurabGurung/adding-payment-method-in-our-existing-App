3aca7762c91e5ccac0d1da5b7e1c70acf45c8f4a
Gaurab Gurung <gaurabgurung@192-168-1-101.tpgi.com.au>
2023-11-26 14:17:00 +1100

    adding stripe utitlity to our app

--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import { Provider } from 'react-redux';
 import { store, persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
+import { Elements } from '@stripe/react-stripe-js';
 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider  loading= {null} store={store}>
       <PersistGate persistor= {persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
