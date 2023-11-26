501c6d03bdfb1ebb75171a51d6dc3545f800968c
Gaurab Gurung <gaurabgurung@192-168-1-101.tpgi.com.au>
2023-11-26 23:25:03 +1100

    adding payment option to our app- inprogress

---
+++ src/components/payment-form/payment-form.component.jsx
@@ -0,0 +1,19 @@
+import { CardElement} from "@stripe/react-stripe-js";
+
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
+
+const PaymentForm = () => {
+
+    return (
+        <PaymentFormContainer>
+            <FormContainer>
+                <h2>Credit Cart Payment: </h2>
+                <CardElement />
+                <Button buttonType= {BUTTON_TYPE_CLASSES.inverted}> Pay Now</Button>
+            </FormContainer>
+        </PaymentFormContainer>
+    )
+}
+
+export default PaymentForm;
--- src/routes/check-out/check-out.component.jsx
+++ src/routes/check-out/check-out.component.jsx
@@ -3,11 +3,14 @@ import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import { useSelector } from 'react-redux';
+import PaymentForm from '../../components/payment-form/payment-form.component';
 
 const CheckOut = () => {
 
     const cartItems = useSelector(selectCartItems);
     const cartTotal = useSelector(selectCartTotal);
+
+    
 
     return (
         <div className='checkout-container'>
@@ -32,6 +35,7 @@ const CheckOut = () => {
                 <CheckoutItem key={cartItem.id} cartItem= {cartItem}/>
             )}
             <span className='total'> Total : ${cartTotal} </span>
+            <PaymentForm />
         </div>
     )
 }
