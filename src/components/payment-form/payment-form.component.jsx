4213d8111d4de6577ac251723598a48bdf65720d
Gaurab Gurung <gaurabgurung@192-168-1-101.tpgi.com.au>
2023-11-27 23:47:45 +1100

    adding stipe functions

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,10 +1,23 @@
-import { CardElement} from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements} from "@stripe/react-stripe-js";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
 
 const PaymentForm = () => {
 
+    const stripe = useStripe();
+    const element = useElements();
+
+    const paymentHandler = async (e) => {
+        e.preventDefault();
+
+        if(!stripe || !element) {
+            return;
+        }
+
+        
+    }
+ 
     return (
         <PaymentFormContainer>
             <FormContainer>
