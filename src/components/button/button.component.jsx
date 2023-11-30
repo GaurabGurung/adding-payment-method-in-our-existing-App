3b3635878bc62e1ba95eaeb8c607b3be2287296e
Gaurab Gurung <gaurabgurung181@gmail.com>
2023-11-30 23:22:24 +1100

    finished adding payment method using stripe and netlify functions

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,4 +1,4 @@
-import { BaseBotton, GoogleSignInBotton, InvertedButton } from './button.styles';
+import { BaseBotton, ButtonSpinner, GoogleSignInBotton, InvertedButton } from './button.styles';
 
 export const BUTTON_TYPE_CLASSES = {
     base: 'base',
@@ -14,13 +14,13 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base ) =>
     }[buttonType])
 
 
-const Button = ({children, buttonType, ...otherProps}) => {
+const Button = ({children, buttonType, isLoading, ...otherProps}) => {
 
     const CustomButton = getButton(buttonType);
 
     return (
-        <CustomButton {...otherProps}> 
-        {children} 
+        <CustomButton disabled= {isLoading} {...otherProps}> 
+            {isLoading ? <ButtonSpinner/> : children} 
         </CustomButton> 
     )
 }
