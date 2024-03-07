4d34a5d364acbc0978bec9e66515a80b659fdd11
Gaurab Gurung <gaurabgurung181@gmail.com>
2024-03-08 01:12:14 +1100

    added scrolling bottom for the app

--- server/app.js
+++ server/app.js
@@ -1,39 +1,39 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const stripe = require("stripe")(
-  "sk_test_51OGMJWKUEZNEGbibVI1jBRY8eqG4Da8Ni3cIn8napcr358nGXxteRMGnKoqDJQCuQjw1tCeQw6FFYf8iJRSTI8KW00X3cT1spA"
-);
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const stripe = require("stripe")(
+//   "sk_test_51OGMJWKUEZNEGbibVI1jBRY8eqG4Da8Ni3cIn8napcr358nGXxteRMGnKoqDJQCuQjw1tCeQw6FFYf8iJRSTI8KW00X3cT1spA"
+// );
 
-app.use(express.json());
-app.use(cors());
+// app.use(express.json());
+// app.use(cors());
 
-//checkout api
+// //checkout api
 
-app.post("/api/create-checkout-session", async (req, res) => {
-  const { products } = req.body;
-  const lineItems = products.map((product) => ({
-    price_data: {
-      currency: "aud",
-      product_data: {
-        name: product.name,
-      },
-      unit_amount: parseInt(product.price, 10) * 100,
-    },
-    quantity: product.quantity,
-  }));
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: lineItems,
-    mode: "payment",
-    success_url: "http://localhost:3000/success",
-    cancel_url: "http://localhost:3000/cancel",
-  });
-  res.json({ id: session.id });
+// app.post("/api/create-checkout-session", async (req, res) => {
+//   const { products } = req.body;
+//   const lineItems = products.map((product) => ({
+//     price_data: {
+//       currency: "aud",
+//       product_data: {
+//         name: product.name,
+//       },
+//       unit_amount: parseInt(product.price, 10) * 100,
+//     },
+//     quantity: product.quantity,
+//   }));
+//   const session = await stripe.checkout.sessions.create({
+//     payment_method_types: ["card"],
+//     line_items: lineItems,
+//     mode: "payment",
+//     success_url: "http://localhost:3000/success",
+//     cancel_url: "http://localhost:3000/cancel",
+//   });
+//   res.json({ id: session.id });
 
-  // console.log(products);
-});
+//   // console.log(products);
+// });
 
-app.listen(8000, () => {
-  console.log("server started");
-});
+// app.listen(8000, () => {
+//   console.log("server started");
+// });
