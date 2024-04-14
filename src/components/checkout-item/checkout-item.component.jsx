0c2a2d70951230853f4145871ebb69b933175150
Gaurab Gurung <gaurabgurung181@gmail.com>
2024-04-14 17:38:31 +1000

    fixed some errors

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -25,20 +25,17 @@ const CheckoutItem = ({ cartItem }) => {
       </div>
       <span className="name"> {name} </span>
       <span className="quantity">
-        <div className="arrow" onClick={removeItemHandler}>
-          {" "}
-          &#10094;{" "}
-        </div>
+        <span className="arrow_handler" onClick={removeItemHandler}>
+          &#10094;
+        </span>
         <span className="value"> {quantity} </span>
-        <div className="arrow" onClick={addItemHandler}>
-          {" "}
-          &#10095;{" "}
-        </div>
+        <span className="arrow_handler" onClick={addItemHandler}>
+          &#10095;
+        </span>
       </span>
       <span className="price">$ {price} </span>
       <div className="remove-button" onClick={clearItemHandler}>
-        {" "}
-        &#10005;{" "}
+        &#10005;
       </div>
     </div>
   );
